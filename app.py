2110aa58ab03680b49d870d63f2f3178e5533e24
Arturofor2014 <81761680+Arturofor2014@users.noreply.github.com>
2026-05-19 19:07:37 -0500

    app.py

--- app.py
+++ app.py
@@ -71,11 +71,11 @@ with tab1:
     st.markdown("#### Navega la presentación directamente aquí")
     if CANVA_URL:
         st.markdown(f"""
-        <div style="position:relative;width:100%;height:0;padding-top:141.4286%;
+        <div style="width:606px;height:567px;
              box-shadow:0 2px 8px 0 rgba(63,69,81,0.16);margin-bottom:0.9em;
-             overflow:hidden;border-radius:8px;will-change:transform;">
+             overflow:hidden;border-radius:8px;">
           <iframe loading="lazy"
-            style="position:absolute;width:100%;height:100%;top:0;left:0;border:none;padding:0;margin:0;"
+            style="width:100%;height:100%;border:none;padding:0;margin:0;"
             src="{CANVA_URL}"
             allowfullscreen="allowfullscreen" allow="fullscreen">
           </iframe>
